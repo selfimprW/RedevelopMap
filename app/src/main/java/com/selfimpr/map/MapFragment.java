09df1f9654912421cde9e982f03dfae05e6053b3
wangjiacheng <wangjiacheng@58ganji.com>
2017-09-14 21:44:05 +0800

    不知为何定位没有回调？？？？？？

--- app/src/main/java/com/selfimpr/map/MapFragment.java
+++ app/src/main/java/com/selfimpr/map/MapFragment.java
@@ -1,8 +1,11 @@
 package com.selfimpr.map;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -13,6 +16,11 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.tencent.map.geolocation.TencentLocation;
+import com.tencent.map.geolocation.TencentLocationListener;
+import com.tencent.map.geolocation.TencentLocationManager;
+import com.tencent.map.geolocation.TencentLocationRequest;
+import com.tencent.map.geolocation.TencentPoi;
 import com.tencent.mapsdk.raster.model.BitmapDescriptorFactory;
 import com.tencent.mapsdk.raster.model.CameraPosition;
 import com.tencent.mapsdk.raster.model.LatLng;
@@ -21,6 +29,8 @@ import com.tencent.mapsdk.raster.model.MarkerOptions;
 import com.tencent.tencentmap.mapsdk.map.MapView;
 import com.tencent.tencentmap.mapsdk.map.TencentMap;
 import com.tencent.tencentmap.mapsdk.map.UiSettings;
+
+import java.util.List;
 
 /**
  * description：map相关
@@ -34,13 +44,24 @@ import com.tencent.tencentmap.mapsdk.map.UiSettings;
  */
 public class MapFragment extends Fragment {
     public static final int TENCENT_MAP_ZOOM = 16;
-    private static final String LOCATION = "android.permission.ACCESS_COARSE_LOCATION";
     private double uLatitude = 39.987221;
     private double uLongitude = 116.505203;
     private boolean isMapLoaded = false; //地图是否加载完成
     private MapView mMapView;
     private TencentMap tencentMap;
     private FragmentActivity activity;
+
+    private static final int[] LEVELS = new int[]{
+            //定位结果信息级别: 0号定位接口, 仅包含经纬度坐标表示的地位置(经纬度).
+            TencentLocationRequest.REQUEST_LEVEL_GEO,
+            //定位结果信息级别: 1号定位接口, 包含经纬度, 位置名称, 位置地址.
+            TencentLocationRequest.REQUEST_LEVEL_NAME,
+            //定位结果信息级别: 3号定位接口, 包含经纬度, 行政区划，位置地址和位置名称.
+            TencentLocationRequest.REQUEST_LEVEL_ADMIN_AREA,
+            //定位结果信息级别: 4号定位接口, 包含经纬度, 行政区划, 附近的POI.
+            TencentLocationRequest.REQUEST_LEVEL_POI};
+    private static final int DEFAULT = 2;
+    private int mLevel = LEVELS[DEFAULT];
 
     @Override
     public void onAttach(Context context) {
@@ -51,6 +72,16 @@ public class MapFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String[] permissions = {
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.READ_PHONE_STATE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            };
+            if (activity.checkSelfPermission(permissions[0]) == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(permissions, 0);
+            }
+        }
     }
 
     @Nullable
@@ -64,6 +95,43 @@ public class MapFragment extends Fragment {
             Toast.makeText(activity, "地图加载失败", Toast.LENGTH_SHORT).show();
         }
         return rootView;
+    }
+
+    private void obtainLocalPosition() {
+        TencentLocationManager mLocationManager = TencentLocationManager.getInstance(activity);
+        // 设置坐标系为 gcj-02, 缺省坐标为 gcj-02, 所以通常不必进行如下调用
+        //mLocationManager.setCoordinateType(TencentLocationManager.COORDINATE_TYPE_GCJ02);
+        // 创建定位请求
+        TencentLocationRequest request = TencentLocationRequest.create()
+                // 当定位周期大于0时, 不论是否有得到新的定位结果, 位置监听器都会按定位周期定时被回调;
+                // 当定位周期等于0时, 仅当有新的定位结果时, 位置监听器才会被回调(即, 回调时机存在不确定性).
+                // 如果需要周期性回调, 建议将 定位周期 设置为 5000-10000ms
+                .setInterval(5 * 1000) // 设置定位周期(位置监听器回调周期), 单位为 ms (毫秒).
+//                .setAllowGPS(true)  //设置是否允许使用GPS进行定位,默认允许. 如果允许使用GPS，在室外可提升定位精度（约3~10米），GPS首次获取位置较慢且耗电较高
+                //.setQQ("1281315018")
+                //不同的 request level 得到的定位结果信息完整程度不同. 通常, 定位结果中所含信息越多消耗的流量也越多, 请选择合理的 request level 以节省流量.
+//                .setRequestLevel(mLevel) // 设置定位level
+                ;
+        // 开始定位
+        mLocationManager.requestLocationUpdates(request, new TencentLocationListener() {
+            @Override
+            public void onLocationChanged(TencentLocation location, int error, String reason) {
+                String msg;
+                if (error == TencentLocation.ERROR_OK) {
+                    // 定位成功
+                    msg = MapFragment.toString(location, mLevel);
+                } else {
+                    // 定位失败
+                    msg = "定位失败: " + reason;
+                }
+                Log.e("wjc", "onLocationChanged:" + msg);
+            }
+
+            @Override
+            public void onStatusUpdate(String s, int i, String s1) {
+                Log.e("wjc", "onStatusUpdate:" + s);
+            }
+        }, Looper.getMainLooper());
     }
 
     private void initTencentMap(FrameLayout layoutContainer) {
@@ -98,7 +166,7 @@ public class MapFragment extends Fragment {
             @Override
             public void onCameraChangeFinish(CameraPosition cameraPosition) {
                 LatLng latLng = cameraPosition.getTarget();
-                if (isMapLoaded && mMapView != null) {
+                if (isMapLoaded) {
                     Log.e("wjc", "onCameraChangeFinish-->lat:" + latLng.getLatitude() + ",lng:" + latLng.getLongitude());
                 }
             }
@@ -118,6 +186,7 @@ public class MapFragment extends Fragment {
             @Override
             public boolean onMarkerClick(Marker marker) {
                 Toast.makeText(activity, "click marker", Toast.LENGTH_SHORT).show();
+                obtainLocalPosition();
                 return false;
             }
         });
@@ -149,8 +218,64 @@ public class MapFragment extends Fragment {
         mMapView.onDestroy();
     }
 
-    private static boolean checkLocationPermission() {
-        int perm = MApplication.getApplication().checkCallingOrSelfPermission(LOCATION);
-        return perm == PackageManager.PERMISSION_GRANTED;
+    // ===== util method
+    private static String toString(TencentLocation location, int level) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("latitude=").append(location.getLatitude()).append(",");
+        sb.append("longitude=").append(location.getLongitude()).append(",");
+        sb.append("altitude=").append(location.getAltitude()).append(",");
+        sb.append("accuracy=").append(location.getAccuracy()).append(",");
+
+        switch (level) {
+            case TencentLocationRequest.REQUEST_LEVEL_GEO:
+                break;
+            case TencentLocationRequest.REQUEST_LEVEL_NAME:
+                sb.append("name=").append(location.getName()).append(",");
+                sb.append("address=").append(location.getAddress()).append(",");
+                break;
+            case TencentLocationRequest.REQUEST_LEVEL_ADMIN_AREA:
+            case TencentLocationRequest.REQUEST_LEVEL_POI:
+            case 7:
+                sb.append("nation=").append(location.getNation()).append(",");
+                sb.append("province=").append(location.getProvince()).append(",");
+                sb.append("city=").append(location.getCity()).append(",");
+                sb.append("district=").append(location.getDistrict()).append(",");
+                sb.append("town=").append(location.getTown()).append(",");
+                sb.append("village=").append(location.getVillage()).append(",");
+                sb.append("street=").append(location.getStreet()).append(",");
+                sb.append("streetNo=").append(location.getStreetNo()).append(",");
+
+                if (level == TencentLocationRequest.REQUEST_LEVEL_POI) {
+                    List<TencentPoi> poiList = location.getPoiList();
+                    int size = poiList.size();
+                    for (int i = 0, limit = 3; i < limit && i < size; i++) {
+                        sb.append("\n").append("poi[" + i + "]=").append(toString(poiList.get(i))).append(",");
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        return sb.toString();
+    }
+
+    private static String toString(TencentPoi poi) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("name=").append(poi.getName()).append(",");
+        sb.append("address=").append(poi.getAddress()).append(",");
+        sb.append("catalog=").append(poi.getCatalog()).append(",");
+        sb.append("distance=").append(poi.getDistance()).append(",");
+        sb.append("latitude=").append(poi.getLatitude()).append(",");
+        sb.append("longitude=").append(poi.getLongitude()).append(",");
+        return sb.toString();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //可在此继续其他操作。
+        Log.e("wjc", "onRequestPermissionsResult");
+        obtainLocalPosition();
     }
 }
